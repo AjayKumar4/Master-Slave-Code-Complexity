f8018da8a3f25d8106625c15f4649348b002a4ea
AJ <n.uajaykumar@gmail.com>
2017-12-10 02:40:36 +0000

    Adding Master and Slave Python file
    Adding Master and Slave Python file

---
+++ Master.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 10 02:38:40 2017
+
+@author: aj
+"""
+
---
+++ Slave.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 10 02:39:29 2017
+
+@author: aj
+"""
+
